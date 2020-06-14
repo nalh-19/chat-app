a34a9a81e827a835e1e20b45bb1135c84b7d16f3
nalh-19 <ntnhan98@gmail.com>
2020-06-14 22:49:16 +0700

    commit

---
+++ client/src/containers/CallPage/index.js
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from "react";
+import { Button, Row, Modal } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import selectors from "./selectors";
+import userSelectors from "../UserPage/selectors";
+import { CallWrapper } from "./style/CallWrapper";
+import AvatarCus from "../../components/AvatarCus";
+import actions from "./actions";
+import {
+    emitCallerCancelRequestCall,
+    emitAnswerCall,
+    emitRejectCall,
+    emitCallEnded,
+} from "./socket";
+import CallingModal from "./CallingModal";
+import ContactingModal from "./ContactingModal";
+import CallCanceledModal from "./CallCanceledModal";
+import StreamModal from "./StreamModal";
+import adapter from "webrtc-adapter";
+
+
+function CallPage() {
+    // console.log(navigator.getUserMedia);
+    // console.log(navigator.webkitGetUserMedia);
+    console.log(adapter.browserDetails.browser);
+    console.log(adapter.browserDetails.version);
+    return (
+        <>
+            <StreamModal />
+            {/* <Answer /> */}
+            <ContactingModal />
+            <CallingModal />
+            <CallCanceledModal />
+        </>
+    );
+}
+
+export default CallPage;
